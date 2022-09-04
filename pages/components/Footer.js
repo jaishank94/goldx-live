ea3d8c77c34114dbdc23e04927d8509311f6fc16
Jai Shankar <jaishank94@gmail.com>
2022-09-05 00:08:39 +0530

    Button Gradient

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -3,7 +3,7 @@ import { useRouter } from "next/router";
 import React, { Fragment, useEffect, useState } from "react";
 import Image from "next/image";
 import { BsTwitter, BsGithub } from "react-icons/bs";
-import GoldBarsImg from "../../public/images/gold-bars.png"
+import GoldBarsImg from "../../public/images/gold-bars.png";
 import Link from "next/link";
 
 const Footer = () => {
@@ -18,19 +18,29 @@ const Footer = () => {
 
   return (
     <Fragment>
-      <footer
-        className='text-center relative bottom-0 w-full pt-48 mt-48 bg-white dark:bg-neutral-800'>
+      <footer className="text-center relative bottom-0 w-full pt-48 mt-48 bg-white dark:bg-neutral-800">
         <div className="max-w-7xl mx-2 absolute -top-36 left-0 right-0 rounded-3xl lg:mx-auto bg-[url('/images/footer-image.jpg')] bg-cover object-fill p-10">
           <div className="flex justify-between">
             <div className="md:w-1/2 text-left">
               <p className="text-3xl md:text-6xl text-white font-bold">
                 Fuse Gold
               </p>
-              <p className="text-white">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi.</p>
-              <button className="rounded-full cursor-pointer border mx-auto mt-6 p-2 text-xl bg-black shadow-lg shadow-inner shadow-gold-400">
+              <p className="text-white">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                quae ab illo inventore veritatis et quasi.
+              </p>
+              {/* <button className="rounded-full cursor-pointer border mx-auto mt-6 p-2 text-xl bg-black shadow-lg shadow-inner shadow-gold-400">
                 <span className="p-2 text-sm text-transparent uppercase font-bold bg-clip-text bg-gradient-to-r from-gold-300 to-gold-100">
                   More about fuse gold
                 </span>
+              </button> */}
+              <button className="rounded-full cursor-pointer pb-[1.8px] mx-auto pb-[0.8px] text-xl bg-gradient-to-r from-gold-200 to-gold-100 ">
+                <div className="rounded-full bg-black p-2">
+                  <span className="text-sm text-transparent font-bold bg-clip-text bg-gradient-to-r from-gold-300 to-gold-100">
+                    More about fuse gold
+                  </span>
+                </div>
               </button>
             </div>
             <div className="hidden md:block absolute right-0 -top-6">
@@ -47,12 +57,12 @@ const Footer = () => {
           </div>
           <ul className="flex justify-center p-2 w-full flex-wrap cursor-pointer items-center mt-3 text-xs font-medium text-gray-500 dark:text-gray-400 sm:mt-0 uppercase border-none">
             {[
-              ['Valuation', '/valuation'],
-              ['Strategic Asset', '/strategic-asset'],
-              ['Gold News', '/gold-news'],
-              ['Blogs', '/blogs'],
-              ['Advertise', '/advertise'],
-              ['Privacy Policy', '/privacy'],
+              ["Valuation", "/valuation"],
+              ["Strategic Asset", "/strategic-asset"],
+              ["Gold News", "/gold-news"],
+              ["Blogs", "/blogs"],
+              ["Advertise", "/advertise"],
+              ["Privacy Policy", "/privacy"],
               // ['Terms & Conditions', '/tnc'],
             ].map(([title, url], k) => (
               <Link href={url} key={k}>
@@ -66,13 +76,12 @@ const Footer = () => {
       </footer>
       <div className="w-full py-2 bg-black items-center bg-[#EDF1F4] dark:bg-neutral-900 ">
         <div className="cursor-pointer text-sm text-gray-500 text-center uppercase">
-          © {new Date().getFullYear()} Gold.Live. All Rights
-          Reserved
+          © {new Date().getFullYear()} Gold.Live. All Rights Reserved
         </div>
       </div>
       <div className="bg-repeat-space bg-gradient-to-r from-gold-300 to-gold-100 h-2"></div>
     </Fragment>
   );
-}
+};
 
-export default Footer
+export default Footer;
--- pages/index.js
+++ pages/index.js
@@ -7,57 +7,57 @@ import { useMoralis } from "react-moralis";
 import Footer from "./components/Footer";
 import moment from "moment";
 import { useTheme } from "next-themes";
-import { BsBell } from 'react-icons/bs';
-import { BiChevronDown } from 'react-icons/bi';
-import { GoArrowUp } from 'react-icons/go';
+import { BsBell } from "react-icons/bs";
+import { BiChevronDown } from "react-icons/bi";
+import { GoArrowUp } from "react-icons/go";
 import SelectField from "./components/SelectField";
-import GoldChartDark from '../public/images/gold-graph-dark.png'
+import GoldChartDark from "../public/images/gold-graph-dark.png";
 
 const index = () => {
   const { theme, setTheme } = useTheme("light");
   const [isMounted, setMounted] = useState(false);
   const weightOptions = [
-    {
-      name: 'Kilogram',
-      value: 'kg'
-    },
-    {
-      name: 'Grams',
-      value: 'grams'
-    }
-  ],
+      {
+        name: "Kilogram",
+        value: "kg",
+      },
+      {
+        name: "Grams",
+        value: "grams",
+      },
+    ],
     currencyOptions = [
       {
-        name: 'USD',
-        value: 'USD'
+        name: "USD",
+        value: "USD",
       },
       {
-        name: 'INR',
-        value: 'INR'
+        name: "INR",
+        value: "INR",
       },
       {
-        name: 'GBP',
-        value: 'GBP'
-      }
+        name: "GBP",
+        value: "GBP",
+      },
     ],
     intervalOptions = [
       {
-        name: '1 Day',
-        value: '1D'
+        name: "1 Day",
+        value: "1D",
       },
       {
-        name: '2 Days',
-        value: '2D'
+        name: "2 Days",
+        value: "2D",
       },
       {
-        name: '3 Days',
-        value: '3D'
+        name: "3 Days",
+        value: "3D",
       },
-    ]
+    ];
 
   useEffect(() => {
     setMounted(true);
-    setTheme('light')
+    setTheme("light");
   }, []);
 
   const handleSidebar = () => {
@@ -80,25 +80,49 @@ const index = () => {
             <div className="relative lg:col-span-3 p-4 rounded-2xl bg-[url('/images/homePgGoldBarWCoins.png')] bg-cover object-fill pt-72">
               <div className="rounded-xl bg-black bg-opacity-75 backdrop-blur-md border border-slate-800 p-4">
                 <div className="flex justify-between items-center text-xs border-b border-slate-800 pb-3 mb-3">
-                  <label className="flex items-center text-white"><span>Real time capital.com</span> <BiChevronDown /></label>
+                  <label className="flex items-center text-white">
+                    <span>Real time capital.com</span> <BiChevronDown />
+                  </label>
                   <div className="flex text-slate-400">
-                    <label>+ Add to Watchlist</label> <span className="px-2">|</span> <label className="flex items-center"><BsBell className="mr-2" /> Create Alert</label>
+                    <label>+ Add to Watchlist</label>{" "}
+                    <span className="px-2">|</span>{" "}
+                    <label className="flex items-center">
+                      <BsBell className="mr-2" /> Create Alert
+                    </label>
                   </div>
                 </div>
                 <div className="flex content-start border-b border-slate-800 pb-3 mb-3">
                   <GoArrowUp className="text-green-600 text-3xl" />
                   <div className="mx-2">
-                    <p className="text-slate-500 text-sm"><span className="text-green-600 font-bold text-2xl">1,770.05</span><span>/USD</span> <span className="text-green-600 ml-3">+8.55 <span className="text-white text-sm">(+0.49%)</span></span>
+                    <p className="text-slate-500 text-sm">
+                      <span className="text-green-600 font-bold text-2xl">
+                        1,770.05
+                      </span>
+                      <span>/USD</span>{" "}
+                      <span className="text-green-600 ml-3">
+                        +8.55{" "}
+                        <span className="text-white text-sm">(+0.49%)</span>
+                      </span>
                     </p>
-                    <p className="text-xs text-slate-300">00:50:30 - Real-time derived data. Currency in USD (Disclaimer)</p>
+                    <p className="text-xs text-slate-300">
+                      00:50:30 - Real-time derived data. Currency in USD
+                      (Disclaimer)
+                    </p>
                   </div>
                 </div>
                 <div className="text-xs text-slate-500">
-                  <label>Prev. Close: <span className="text-white">1,761.50</span></label>
+                  <label>
+                    Prev. Close: <span className="text-white">1,761.50</span>
+                  </label>
                   <span className="mx-3">|</span>
-                  <label>Open: <span className="text-white">1,764.45</span></label>
+                  <label>
+                    Open: <span className="text-white">1,764.45</span>
+                  </label>
                   <span className="mx-3">|</span>
-                  <label>Day's Range: <span className="text-white">1,763.00 - 1,770.55</span></label>
+                  <label>
+                    Day's Range:{" "}
+                    <span className="text-white">1,763.00 - 1,770.55</span>
+                  </label>
                 </div>
               </div>
             </div>
@@ -107,19 +131,37 @@ const index = () => {
                 <div className="flex justify-between items-center">
                   <div className="">
                     <p className="text-2xl font-medium">Fuse Gold (FUSEG)</p>
-                    <p className="flex text-base font-medium text-inherit">Spot price: $1,770.05 <span className="flex text-green-600 text-base"><GoArrowUp /> +8.55 <span>(+0.49%)</span></span></p>
+                    <p className="flex text-base font-medium text-inherit">
+                      Spot price: $1,770.05{" "}
+                      <span className="flex text-green-600 text-base">
+                        <GoArrowUp /> +8.55 <span>(+0.49%)</span>
+                      </span>
+                    </p>
                   </div>
                   <button className="cursor-pointer border p-1 text-sm bg-gray-400 rounded-full m-1 border-slate-200 dark:border-neutral-300 hover:border-slate-400">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                      />
                     </svg>
                   </button>
                 </div>
                 <div className="mt-20">
-                  <button className="rounded-full cursor-pointer border mx-auto p-2 text-xl bg-black shadow-lg shadow-gold-400">
-                    <span className="p-2 text-sm text-transparent font-bold bg-clip-text bg-gradient-to-r from-gold-300 to-gold-100">
-                      BUY NOW
-                    </span>
+                  <button className="rounded-full cursor-pointer mx-auto pb-[0.8px] text-xl bg-gradient-to-r from-gold-300 to-gold-100 ">
+                    <div className="rounded-full bg-black p-2">
+                      <span className="text-sm text-transparent font-bold bg-clip-text bg-gradient-to-r from-gold-300 to-gold-100">
+                        BUY NOW
+                      </span>
+                    </div>
                   </button>
                 </div>
               </div>
@@ -127,19 +169,37 @@ const index = () => {
                 <div className="flex justify-between items-center">
                   <div className="">
                     <p className="text-2xl font-medium">Pax Gold (PAXG)</p>
-                    <p className="flex text-base font-medium text-inherit">Spot price: $1,770.05 <span className="flex text-green-600 text-base"><GoArrowUp /> +8.55 <span>(+0.49%)</span></span></p>
+                    <p className="flex text-base font-medium text-inherit">
+                      Spot price: $1,770.05{" "}
+                      <span className="flex text-green-600 text-base">
+                        <GoArrowUp /> +8.55 <span>(+0.49%)</span>
+                      </span>
+                    </p>
                   </div>
                   <button className="cursor-pointer border p-1 text-sm bg-gray-400 rounded-full m-1 border-slate-200 dark:border-neutral-300 hover:border-slate-400">
-                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                      <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      strokeWidth="1.5"
+                      stroke="currentColor"
+                      className="w-6 h-6"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M8.25 4.5l7.5 7.5-7.5 7.5"
+                      />
                     </svg>
                   </button>
                 </div>
                 <div className="mt-20">
-                  <button className="rounded-full cursor-pointer border mx-auto p-2 text-xl bg-black shadow-lg shadow-inner shadow-gold-400">
-                    <span className="p-2 text-sm text-transparent font-bold bg-clip-text bg-gradient-to-r from-gold-300 to-gold-100">
-                      BUY NOW
-                    </span>
+                  <button className="rounded-full cursor-pointer mx-auto pb-[0.8px] text-xl bg-gradient-to-r from-gold-300 to-gold-100 ">
+                    <div className="rounded-full bg-black p-2">
+                      <span className="text-sm text-transparent font-bold bg-clip-text bg-gradient-to-r from-gold-300 to-gold-100">
+                        BUY NOW
+                      </span>
+                    </div>
                   </button>
                 </div>
               </div>
@@ -149,12 +209,17 @@ const index = () => {
                 <p className="text-2xl text-white font-bold">
                   Lorem ipsum dolor sit amet consectetur.
                 </p>
-                <p className="text-slate-400">Sed ut perspiciatis unde omnis iste natus error sit voluptatem totam.</p>
-                <button className="rounded-full cursor-pointer border mx-auto mt-6 p-2 text-xl bg-black shadow-lg shadow-inner shadow-gold-400">
-                  <span className="p-2 text-sm text-transparent uppercase font-bold bg-clip-text bg-gradient-to-r from-gold-300 to-gold-100">
-                    Learn More
-                  </span>
-                </button>
+                <p className="text-slate-400">
+                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                  totam.
+                </p>
+                <button className="rounded-full cursor-pointer mx-auto pb-[0.8px] text-xl bg-gradient-to-r from-gold-300 to-gold-100 ">
+                    <div className="rounded-full bg-black p-2">
+                      <span className="text-sm text-transparent font-bold bg-clip-text bg-gradient-to-r from-gold-300 to-gold-100">
+                        LEARN MORE
+                      </span>
+                    </div>
+                  </button>
               </div>
             </div>
             <div className="md:relative lg:col-span-3 p-6 rounded-3xl bg-[url('/images/home-page-gold-in-hand.png')] bg-cover object-fill">
@@ -162,12 +227,17 @@ const index = () => {
                 <p className="text-2xl text-white font-bold">
                   Lorem ipsum dolor sit amet consectetur.
                 </p>
-                <p className="text-slate-400">Sed ut perspiciatis unde omnis iste natus error sit voluptatem totam.</p>
-                <button className="rounded-full cursor-pointer border mx-auto mt-6 p-2 text-xl bg-black shadow-lg shadow-inner shadow-gold-400">
-                  <span className="p-2 text-sm text-transparent uppercase font-bold bg-clip-text bg-gradient-to-r from-gold-300 to-gold-100">
-                    Learn More
-                  </span>
-                </button>
+                <p className="text-slate-400">
+                  Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                  totam.
+                </p>
+                <button className="rounded-full cursor-pointer mx-auto pb-[0.8px] text-xl bg-gradient-to-r from-gold-300 to-gold-100 ">
+                    <div className="rounded-full bg-black p-2">
+                      <span className="text-sm text-transparent font-bold bg-clip-text bg-gradient-to-r from-gold-300 to-gold-100">
+                        LEARN MORE
+                      </span>
+                    </div>
+                  </button>
               </div>
             </div>
           </div>
@@ -176,14 +246,16 @@ const index = () => {
           </div>
           <div className="">
             <div className="md:flex align-center justify-between mb-10">
-              <p className="text-xl md:text-2xl font-medium my-auto">Gold History</p>
+              <p className="text-xl md:text-2xl font-medium my-auto">
+                Gold History
+              </p>
               <div className="flex items-center">
                 <SelectField options={weightOptions} />
                 <SelectField options={currencyOptions} />
                 <SelectField options={intervalOptions} />
               </div>
             </div>
-            <Image src={GoldChartDark} layout={'responsive'} />
+            <Image src={GoldChartDark} layout={"responsive"} />
           </div>
           <div className="max-w-6xl mx-auto border border-slate-300 uppercase w-full my-20 h-24 flex items-center justify-center">
             ** Google Ads **
@@ -193,12 +265,23 @@ const index = () => {
               <p className="text-3xl md:text-6xl text-white font-bold">
                 Lorem ipsum dolor sit amet consectetur.
               </p>
-              <p className="text-slate-400">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi.</p>
-              <button className="rounded-full cursor-pointer border mx-auto mt-6 p-2 text-xl bg-black shadow-lg shadow-inner shadow-gold-400">
+              <p className="text-slate-400 pb-4">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem
+                accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
+                quae ab illo inventore veritatis et quasi.
+              </p>
+              {/* <button className="rounded-full cursor-pointer border mx-auto mt-6 p-2 text-xl bg-black shadow-lg shadow-inner shadow-gold-400">
                 <span className="p-2 text-sm text-transparent uppercase font-bold bg-clip-text bg-gradient-to-r from-gold-300 to-gold-100">
-                  Learn More
+                  LEARN MORE
                 </span>
-              </button>
+              </button> */}
+              <button className="rounded-full cursor-pointer mx-auto pb-[0.8px] text-xl bg-gradient-to-r from-gold-300 to-gold-100 ">
+                    <div className="rounded-full p-2">
+                      <span className="text-sm text-white font-bold bg-gradient-to-r from-gold-300 to-gold-100">
+                        LEARN MORE
+                      </span>
+                    </div>
+                  </button>
             </div>
           </div>
           <div className="max-w-6xl mx-auto border border-slate-300 uppercase w-full my-20 h-24 flex items-center justify-center">
@@ -209,6 +292,6 @@ const index = () => {
       </div>
     </Fragment>
   );
-}
+};
 
-export default index
+export default index;
